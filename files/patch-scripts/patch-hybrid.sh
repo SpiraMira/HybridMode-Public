e89ccd00624fb361032592da21e7bd8d38e9e2bf
Patrice Kouame <pkouame@mac.com>
2018-12-26 13:09:15 -0500

    remove install scripts

--- files/patch-scripts/patch-hybrid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#title            :patch_hybrid.sh
-#description      :some fixes for Mojave Light Mode for unupported macs 
-#date             :20181006
-#version          :0.01
-#description      :A script that patches AppKit in-place to present some Light Mode vibrancy workarounds (preserving it in some key areas and cleaning up the glitches in others) 
-#
-#
-# A PLACE HOLDER FOR THE COMBO PATCH TO SIMULATE A VIBRANT LIGHT MODE
